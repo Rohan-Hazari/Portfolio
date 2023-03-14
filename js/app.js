debcbce9b84774235b561c87fb052be0ddbf0003
Rohan Hazari <rohanhazari2012@gmail.com>
2023-03-14 22:23:51 +0530

    Added Project section

--- js/app.js
+++ js/app.js
@@ -1,16 +1,22 @@
-
-//does this appear in github?
-
 const togglebtn = document.querySelector(".toggle-btn");
 const navbar = document.querySelector(".navbar");
 const links = document.querySelector(".links");
 
-togglebtn.addEventListener("click",function(){
-    console.log(links.classList);
-    console.log(navbar.classList);
-    links.classList.toggle("show-links");
-    navbar.classList.toggle("show-navbarpad");
-
+togglebtn.addEventListener("click", function () {
+  console.log(links.classList);
+  console.log(navbar.classList);
+  links.classList.toggle("show-links");
+  navbar.classList.toggle("show-navbarpad");
 });
 
+const preloader = document.querySelector(".preloader");
+const body = document.querySelector(".sub-body");
 
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+  body.style.display = "none";
+  setTimeout(() => {
+    preloader.style.display = "none";
+    body.style.display = "block";
+  }, 1500);
+});
